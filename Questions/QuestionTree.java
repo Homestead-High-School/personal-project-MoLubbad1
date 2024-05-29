2d88217f4901dc21332b85813ca167621a77d44f
Mo <mohammed.b.lubbad@gmail.com>
2024-05-29 08:42:02 -0500

    Read

--- Questions/QuestionTree.java
+++ Questions/QuestionTree.java
@@ -11,19 +11,22 @@ public class QuestionTree{
         while(input.hasNextLine()){ 
             String now = input.nextLine(); 
             if(input.hasNextLine()){
-                String next = input.nextLine().trim(); 
-                root = new QuestionNode(now, readHelper(now, next), readHelper(now, next), null);
+                root = new QuestionNode(now,null,null,null); //Will create the root node
+                root.yes = readHelper(now, input, root); //Will build up the left/yes node
+                root.no = readHelper(now, input, root); //Will build up the right/no node
             }
         }
     } 
     //Method to help build the trees in the read method
-    public QuestionNode readHelper(String now, String next){ 
-        if(now.equals("Q:")){ 
-            QuestionNode x = new QuestionNode(next, readHelper(now, next), readHelper(now.nextLine().nextLine(), next.nextLine().nextLine()), now);
-        } 
-        else if(now.equals("A:")){ 
-            QuestionNode y = new QuestionNode(next, null, null, now);
-        }
+    public QuestionNode readHelper(String now, Scanner input, QuestionNode parent){  
+        QuestionNode leaf; 
+        String next = input.nextLine().trim(); 
+        leaf = new QuestionNode(next,null,null, parent); //Builds the node as if it is an answer
+        if(now.equals("Q:")){ //If it is a question build the yes and no nodes
+            leaf.yes=readHelper(now, input, leaf);
+            leaf.no=readHelper(now, input, leaf);
+        }   
+        return leaf; 
     }
 
     public void write(PrintStream output){ 
@@ -37,6 +40,7 @@ public class QuestionTree{
     // returns true if the answer was yes, returns false otherwise
     public boolean yesTo(String prompt){ 
         System.out.print(prompt + " (y/n)? ");
+        Scanner console = new Scanner(System.in);
         String response = console.nextLine().trim().toLowerCase();
         while (!response.equals("y") && !response.equals("n")) {
             System.out.println("Please answer y or n.");
